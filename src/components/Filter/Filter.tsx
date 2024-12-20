125a905baa166f6203e419999ebcfe5e7e5df580
Tim Spektrov <tspektrov@mail.ru>
2024-12-20 10:43:40 +0300

    update mobile filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -6,6 +6,7 @@ import {STOPS_ALL, TCurrency, TTransferStops} from "../../types/ticket.ts";
 import {FilterCurrency} from "./FilterCurrency";
 import {FilterStops} from "./FilterStops";
 import cn from "classnames";
+import {FilterButton} from "./FilterButton/FilterButton.tsx";
 
 const buttonGroup: IFilterButton[] = [
   {
@@ -47,14 +48,14 @@ const checkboxGroup: IFilterCheckbox[] = [
 ]
 export const Filter: FC = () => {
   const [isFilterVisible, setIsFilterVisible] = useState<boolean>(true);
-  const [_, setLastScrollY] = useState<number>(0);
+  const [lastScrollY, setLastScrollY] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState(true)
   const {currency, setNewCurrency, transferStops, setNewTransferStops} = useTicket()
+  
   const handleClickCurrency = (value: TCurrency) => {
     setNewCurrency(value)
   }
-
   const handleChangeStops = (value: TTransferStops, only: boolean | undefined) => {
-    console.log(value, only)
       setNewTransferStops(value, only)
   }
   const handleClickStops = (value: TTransferStops) => {
@@ -64,15 +65,11 @@ export const Filter: FC = () => {
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
+      if (currentScrollY === lastScrollY) return
       setLastScrollY(prevLastScrollY => {
-        if (currentScrollY > prevLastScrollY) {
-          setIsFilterVisible(false); // Скролл вниз
-        } else {
-          setIsFilterVisible(true); // Скролл вверх
-        }
-        return currentScrollY; // Возвращаем новое значение для lastScrollY
-      });
-        setLastScrollY(currentScrollY);
+        setIsFilterVisible(currentScrollY < prevLastScrollY)
+        return currentScrollY
+      })
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -82,8 +79,32 @@ export const Filter: FC = () => {
     };
   }, []);
 
+  const handleFilterButtonClick = () => {
+    setIsFilterVisible(true)
+    setIsOpen(!isOpen)
+  }
   return(
-    <div className={cn(styles.filter, {[styles.open]: isFilterVisible})}>
+    <div className={cn(
+      styles.filter,
+      {
+        [styles.hidden]: !isFilterVisible,
+        [styles.open]: isOpen,
+      })}>
+      <button className={styles.button} onClick={handleFilterButtonClick}>
+        <svg className={styles['filter-button']} version="1.0" xmlns="http://www.w3.org/2000/svg"
+             width="512" height="512" viewBox="-20 -20 550 550"
+             preserveAspectRatio="xMidYMid meet">
+
+          <g transform="translate(0,512) scale(0.1,-0.1)"
+             fill={isOpen ? 'currentColor' : 'transparent'} stroke="currentColor" strokeWidth="300">
+            <path d="M210 5114 c-70 -11 -101 -27 -139 -71 -64 -72 -86 -167 -56 -241 11
+    -26 286 -307 930 -952 l915 -915 2 -915 c3 -867 4 -917 21 -950 23 -42 990
+    -1015 1032 -1037 109 -59 264 -12 317 95 l23 47 3 1380 2 1380 915 915 c644
+    645 919 926 930 952 8 21 15 50 15 66 0 52 -29 127 -67 171 -71 84 177 76
+    -2463 77 -1295 1 -2366 0 -2380 -2z"/>
+          </g>
+        </svg>
+      </button>
       <FilterCurrency buttons={buttonGroup} currency={currency} onClick={handleClickCurrency} />
       <FilterStops checkboxes={checkboxGroup} transferStops={transferStops} onChange={handleChangeStops} onClick={handleClickStops}/>
     </div>
